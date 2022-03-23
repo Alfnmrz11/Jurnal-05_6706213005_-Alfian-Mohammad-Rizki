2f9624dad356d83004602df46ba174cf18d6cf03
Alfian Mohammad Rizki <91297351+Alfnmrz11@users.noreply.github.com>
2022-03-23 21:12:14 +0700

    Soal No 2

--- Main.java
+++ Main.java
@@ -1,18 +1,35 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String keluaran;
+        QueueB<String> queue = new QueueB<>();
 
-        if (input.equals("")) {
-            System.out.println("Berikan Kembali inputan Infix !");
-        } else {
-            OperasiHitung theTrans = new OperasiHitung(input.next());
-            keluaran = theTrans.doTrans();
-            System.out.println(keluaran);
+        Scanner input = new Scanner(System.in);
+
+        int pilih = 0;
+        while(pilih != 4){
+            System.out.println("1. Input Data");
+            System.out.println("2. Remove Data");
+            System.out.println("3. Cetak Data");
+            System.out.println("4. Exit");
+            pilih = input.nextInt();
+            if(pilih == 1){
+                System.out.println("Input To Do List");
+                String tDL = input.next(); /* Inputan To Do List*/
+                queue.enqueue(tDL);
+                System.out.println();
+            }
+            else if(pilih == 2){
+                queue.dequeue();
+                System.out.println("Remove Success For First Assignment");
+                System.out.println();
+            }
+            else if(pilih == 3){
+                queue.printQueue();
+                System.out.println();
+            }
         }
     }
 }
